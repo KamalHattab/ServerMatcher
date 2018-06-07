07e528fa21e9fa29d3224e166abe9317cdc50289
Utente <Utente@10.6.68.108>
2018-06-07 14:59:24 +0200

    added files

---
+++ src/server/AbstractServer.java
@@ -0,0 +1,9 @@
+package server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class AbstractServer {
+
+	private static final Logger logger=LoggerFactory.getLogger(AbstractServer.class);
+	
+}
